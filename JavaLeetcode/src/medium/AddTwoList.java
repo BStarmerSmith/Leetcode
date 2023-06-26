a00a99805b9c4f68792659ecc3b19a54c79dd2b6
Ben Starmer-Smith <ben@shadowrobot.com>
2023-06-26 14:16:33 +0100

    Removed :

--- JavaLeetcode/src/medium/AddTwoList.java
+++ JavaLeetcode/src/medium/AddTwoList.java
@@ -6,21 +6,10 @@ public class AddTwoList {
         printListNode(l1_rev);
         ListNode l2_rev = reverseLinkedList(l2);
         printListNode(l2_rev);
-        int l1_sum = sumOfNodes(l1_rev);
-        int l2_sum = sumOfNodes(l2_rev);
-        int result = l1_sum + l2_sum;
-        System.out.printf("\nl1_sum: %d l2_sum: %d result: %d", l1_sum, l2_sum, result);
-        int[] resultArray = splitIntToArray(result);
-        ListNode resultNode = new ListNode(resultArray[0]);
-        ListNode current = resultNode;
-        for (int i = 1; i < resultArray.length; i++) {
-            current.next = new ListNode(resultArray[i]);
-            current = current.next;
-        }
+        ListNode result = sumOfNodes(l1_rev, l2_rev);
         System.out.print("\nresultNode: ");
-        ListNode resultNodeRev = reverseLinkedList(resultNode);
-        printListNode(resultNodeRev);
-        return resultNodeRev;
+        printListNode(result);
+        return result;
     }
 
     private ListNode reverseLinkedList(ListNode head) {
@@ -34,39 +23,46 @@ public class AddTwoList {
             current = next;
         }
 
-        return prev;  // Return the new head of the reversed linked list
+        return prev;
     }
 
-    private int sumOfNodes(ListNode head) {
-        ListNode current = head;
-        String number = "";
+    private ListNode sumOfNodes(ListNode l1, ListNode l2) {
+        System.out.println("sumOfNodes");
+        ListNode dummyHead = new ListNode(0);
+        ListNode current = dummyHead;
+        int carry = 0;
 
-        while (current != null) {
-            number += current.val;
+        while (l1 != null || l2 != null || carry != 0) {
+            int sum = carry;
+            if (l1 != null) {
+                sum += l1.val;
+                l1 = l1.next;
+            }
+            if (l2 != null) {
+                sum += l2.val;
+                l2 = l2.next;
+            }
+            int digit = sum % 10;
+            carry = sum / 10;
+            if (carry != 0){
+                current.next = new ListNode(carry);
+            }
+            current.next = new ListNode(digit);
             current = current.next;
+            System.out.println("sum: " + sum + " digit: " + digit + " carry: " + carry);
         }
-        return Integer.parseInt(number);
-    }
 
-    private int[] splitIntToArray(int number) {
-        int length = String.valueOf(number).length();
-        int[] digits = new int[length];
-
-        for (int i = length - 1; i >= 0; i--) {
-            digits[i] = number % 10;
-            number /= 10;
-        }
-        return digits;
+        return dummyHead.next;
     }
 
     public void printListNode(ListNode head) {
         ListNode current = head;
-        String number = "";
         System.out.print("[");
         while (current != null) {
             System.out.print(current.val);
             current = current.next;
-        }System.out.print("]");
+        }
+        System.out.print("]");
     }
 
     public void testSolution() {
@@ -82,11 +78,17 @@ public class AddTwoList {
         addTwoNumbers(t2_l1, t2_l2);
         System.out.println();
 
-        ListNode t3_l1 = new ListNode(9, new ListNode(9, new ListNode(9, new ListNode(9, new ListNode(9, new ListNode(9, new ListNode(9)))))));
+        ListNode t3_l1 = new ListNode(9,
+                new ListNode(9, new ListNode(9, new ListNode(9, new ListNode(9, new ListNode(9, new ListNode(9)))))));
         ListNode t3_l2 = new ListNode(9, new ListNode(9, new ListNode(9, new ListNode(9))));
         System.out.println("l1 = [9,9,9,9,9,9,9], l2 = [9,9,9,9] 9999999+9999=10009998 output=[8,9,9,9,0,0,0,1]");
         addTwoNumbers(t3_l1, t3_l2);
         System.out.println();
-    }
 
+        ListNode t4_l1 = new ListNode(2, new ListNode(4, new ListNode(9)));
+        ListNode t4_l2 = new ListNode(5, new ListNode(6, new ListNode(4, new ListNode(9))));
+        System.out.println("l1 = [2,4,9], l2 = [5,6,4,9] 942+9645=10587 output=[7,0,4,0,1]");
+        addTwoNumbers(t4_l1, t4_l2);
+        System.out.println();
+    }
 }
