9e0f6c45e49766e288a2934da26483367309ff02
Ben Starmer-Smith <ben@shadowrobot.com>
2023-06-27 11:42:28 +0100

    Added assert tests for java

--- JavaLeetcode/src/medium/AddTwoList.java
+++ JavaLeetcode/src/medium/AddTwoList.java
@@ -4,9 +4,7 @@ public class AddTwoList {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
 
         ListNode result = sumOfNodes(l1, l2);
-        System.out.print("resultNode: ");
-        printListNode(result);
-        return result;
+         return result;
     }
 
     private ListNode sumOfNodes(ListNode l1, ListNode l2) {
@@ -48,27 +46,56 @@ public class AddTwoList {
         ListNode t1_l1 = new ListNode(2, new ListNode(4, new ListNode(3)));
         ListNode t1_l2 = new ListNode(5, new ListNode(6, new ListNode(4)));
         System.out.println("l1 = [2,4,3], l2 = [5,6,4] 342+465=807 output=[7,0,8]");
-        addTwoNumbers(t1_l1, t1_l2);
+        ListNode t1_result = addTwoNumbers(t1_l1, t1_l2);
+        System.out.print("Expected output: [7,0,8] ");
+        printListNode(t1_result);
         System.out.println();
+        assertListNodeEquals(t1_result, new ListNode(7, new ListNode(0, new ListNode(8))));
 
         ListNode t2_l1 = new ListNode(0);
         ListNode t2_l2 = new ListNode(0);
         System.out.println("l1 = [0], l2 = [0] 0+0=0 output=[0]");
-        addTwoNumbers(t2_l1, t2_l2);
+        ListNode t2_result = addTwoNumbers(t2_l1, t2_l2);
+        System.out.print("Expected output: [0] ");
+        printListNode(t2_result);
         System.out.println();
+        assertListNodeEquals(t2_result, new ListNode(0));
 
         ListNode t3_l1 = new ListNode(9,
                 new ListNode(9, new ListNode(9, new ListNode(9, new ListNode(9, new ListNode(9, new ListNode(9)))))));
         ListNode t3_l2 = new ListNode(9, new ListNode(9, new ListNode(9, new ListNode(9))));
         System.out.println("l1 = [9,9,9,9,9,9,9], l2 = [9,9,9,9] 9999999+9999=10009998 output=[8,9,9,9,0,0,0,1]");
-        addTwoNumbers(t3_l1, t3_l2);
+        ListNode t3_result = addTwoNumbers(t3_l1, t3_l2);
+        System.out.print("Expected output: [8,9,9,9,0,0,0,1] ");
+        printListNode(t3_result);
         System.out.println();
+        assertListNodeEquals(t3_result, new ListNode(8, new ListNode(9, new ListNode(9, new ListNode(9, new ListNode(0, new ListNode(0, new ListNode(0, new ListNode(1)))))))));
 
         ListNode t4_l1 = new ListNode(2, new ListNode(4, new ListNode(9)));
         ListNode t4_l2 = new ListNode(5, new ListNode(6, new ListNode(4, new ListNode(9))));
         System.out.println("l1 = [2,4,9], l2 = [5,6,4,9] 942+9645=10587 output=[7,0,4,0,1]");
-        addTwoNumbers(t4_l1, t4_l2);
+        ListNode t4_result = addTwoNumbers(t4_l1, t4_l2);
+        System.out.print("Expected output: [7,0,4,0,1] ");
+        printListNode(t4_result);
         System.out.println();
+        assertListNodeEquals(t4_result, new ListNode(7, new ListNode(0, new ListNode(4, new ListNode(0, new ListNode(1))))));
+    }
+
+    public void assertListNodeEquals(ListNode l1, ListNode l2) {
+        ListNode p1 = l1;
+        ListNode p2 = l2;
+
+        while (p1 != null && p2 != null) {
+            if (p1.val != p2.val) {
+                throw new AssertionError("ListNode values do not match");
+            }
+            p1 = p1.next;
+            p2 = p2.next;
+        }
+
+        if (p1 != null || p2 != null) {
+            throw new AssertionError("ListNode lengths do not match");
+        }
     }
 
 }
